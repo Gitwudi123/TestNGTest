935b0f2db8c41329d49ac46ed34dd82ee07afc7d
Gitwudi123 <15738719051@163.com>
2021-08-19 10:50:26 +0800

    from MyMaven to TestNGTest

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,45 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+public class NewTest {
+
+	 private static Calculator calculator = new Calculator();
+
+	     @BeforeMethod
+	     public void setUp() throws Exception
+	     {
+	      calculator.clear();
+	     }
+
+	     public void testAdd()   {
+	      calculator.add(2);
+	      calculator.add(3);
+	      assertEquals(5, calculator.getResult());
+	     }
+	     @Test
+	     public void testSubstract()
+	     {
+	      calculator.add(10);
+	      calculator.substract(8);
+	      System.out.println(calculator.getResult());
+	      assertEquals(6, calculator.getResult());
+	     }
+
+	    @Test
+	     public void testMultiply() {
+	         System.out.println("");
+	    }
+
+	    @Test
+	    public void testDivide()
+	     {
+	      calculator.add(8);
+	      calculator.divide(2);
+	      assertEquals(4, calculator.getResult());
+	     }
+
+}
